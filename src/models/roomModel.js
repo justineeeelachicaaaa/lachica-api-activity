a197604e66ad43d7c0a738a94d7e1628b08ebb38
unknown <lachica.justine@marsu.edu.ph>
2026-02-10 17:12:20 +0800

    Implemented Validation

--- src/models/roomModel.js
+++ src/models/roomModel.js
@@ -1,25 +1,38 @@
-
 const mongoose = require('mongoose');
-// This is the "Rule Book" for a Room 
+
+//Rule Book
 const roomSchema = new mongoose.Schema({
- roomNumber: {
-    type: Number, // Number: 101
-    required: true,
-    unique: true, 
-},
-type: {
-    type: String, 
-    required: true,
-},
-price: {
-    type: Number,
-    required: true,
-},
-isBooked: {
-    type: Boolean, // True or False
-    default: false, // Assume room is empty (False) at first
-},
-features: [String], // A list of words: ["WiFi", "TV", "Bath"]
+    roomNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+        min: [100, 'Room number must be 3 digits']
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Price cannot be negative']
+    },
+    isBooked: {
+        type: Boolean,
+        default: false,
+    },
+    maintenanceLog: [
+        {
+            date: { type: Date, default:Date.now},
+            issue: String,
+            fixed: Boolean
+        }
+    ],
+    guest: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Guest' // Connects to Guest model
+    },
+    features: [String],
 });
 
 module.exports = mongoose.model('Room', roomSchema);
--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,20 +1,34 @@
-const express = require('express');
-const router = express. Router( ) ;
+const express = require('express' );
+const router = express.Router();
 
-// Import the Controller
+//Import Controller
 const {
-getAllRooms,
-createRoom,
-getRoomById,
-updateRoom,
-deleteRoom,
+  getAllRooms,
+  createRoom,
+  getRoomById,
+  updateRoom,
+  deleteRoom,
 } = require('../controllers/roomController');
 
-// Routes
+const {
+  getAllGuests,
+  createGuest,
+  getGuestById,
+  updateGuest,
+  deleteGuest,
+} = require('../controllers/guestController');
+
+//Routes
 router.get('/rooms', getAllRooms);
 router.post('/rooms', createRoom);
-router.get('/rooms/:id',getRoomById);
+router.get('/rooms/:id', getRoomById);
 router.put('/rooms/:id', updateRoom);
 router.delete('/rooms/:id', deleteRoom);
+
+router.get('/guests', getAllGuests);
+router.post('/guests', createGuest);
+router.get('/guests/:id', getGuestById);
+router.put('/guests/:id', updateGuest);
+router.delete('/guests/:id', deleteGuest);
 
 module.exports = router;
